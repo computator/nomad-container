ce1c914285d0fe169a8e2db6d4bcdb15a7988438
Reuben Lifshay <rlifshay@gmail.com>
2025-12-29 00:22:54 -0800

    feat: build a custom container instead of using the upstream directly

---
+++ Containerfile
@@ -0,0 +1,25 @@
+ARG NOMAD_VERSION=1.11
+FROM docker.io/hashicorp/nomad:${NOMAD_VERSION}
+
+LABEL org.opencontainers.image.source=https://github.com/computator/nomad-container
+
+ARG TARGETOS
+ARG TARGETARCH
+RUN set -eux; \
+	rel_base=https://releases.hashicorp.com ; \
+	rel_url=$( \
+		wget -qO - "${rel_base}$( \
+				wget -qO - "${rel_base}/nomad-driver-podman/" \
+				| grep -Eom 1 '/nomad-driver-podman/[0-9.]+/' \
+			)" \
+			| awk '/data-os="'"${TARGETOS}"'"/ && /data-arch="'"${TARGETARCH}"'"/ { print substr($0, match($0, /https?:[^"]*/), RLENGTH) }' \
+	) ; \
+	wget -O plugin.zip "${rel_url}"; \
+	unzip plugin.zip nomad-driver-podman; \
+	install -D nomad-driver-podman /plugins/nomad-driver-podman; \
+	rm -f nomad-driver-podman plugin.zip
+
+COPY entrypoint.sh /
+
+EXPOSE 4646 4647 4648/tcp 4648/udp
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+set -eu
+
+EP_CONF_DIR=/run/nomad-entrypoint-conf.d
+
+build_configs () {
+	cat > "${EP_CONF_DIR}/disable-updates.hcl" <<-EOF
+		disable_update_check = true
+	EOF
+
+	if [ -n "${ADVERTISE_IP-}" ]; then
+			cat > "${EP_CONF_DIR}/advertise-ip.hcl" <<-EOF
+				advertise {
+				  http = "${ADVERTISE_IP}"
+				  rpc = "${ADVERTISE_IP}"
+				  serf = "${ADVERTISE_IP}"
+				}
+			EOF
+	fi
+
+	if [ -n "${DRIVER_ALLOWLIST-}" ]; then
+			cat > "${EP_CONF_DIR}/driver-allow.hcl" <<-EOF
+				client {
+				  options {
+				    "driver.allowlist" = "${DRIVER_ALLOWLIST}"
+				  }
+				}
+			EOF
+	fi
+
+	if [ -n "${NOMAD_SERVERS-}" ]; then
+			cat > "${EP_CONF_DIR}/servers.hcl" <<-EOF
+				client {
+				  servers = [$(
+						echo "${NOMAD_SERVERS}" \
+						| tr , ' ' \
+						| xargs printf '"%s",' \
+						| sed 's/,$//'
+					)]
+				}
+			EOF
+	fi
+}
+
+mkdir -p ${EP_CONF_DIR}
+
+cmd=${1-}
+[ $# -gt 0 ] && shift
+set -- nomad ${cmd:+"${cmd}"} \
+	-config ${EP_CONF_DIR} \
+	-config /config \
+	-data-dir /data \
+	-alloc-dir /alloc \
+	-plugin-dir /plugins \
+	"$@"
+
+build_configs
+
+exec "$@"
