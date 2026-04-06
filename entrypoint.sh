b5aa3dde6e3d73ee3c796406f6259f55761d2f34
Reuben Lifshay <rlifshay@gmail.com>
2026-04-06 00:20:36 -0700

    chore: correct whitespace in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,84 +15,84 @@ build_configs () {
 	EOF
 
 	if [ -n "${BIND_PORTS-}" ]; then
-			cat > "${EP_CONF_DIR}/ports.hcl" <<-EOF
-				ports {
-				  ${BIND_PORTS}
-				}
-			EOF
+		cat > "${EP_CONF_DIR}/ports.hcl" <<-EOF
+			ports {
+			  ${BIND_PORTS}
+			}
+		EOF
 	fi
 
 	if [ -n "${BIND_ADDR-}" ]; then
-			cat > "${EP_CONF_DIR}/bind-addr.hcl" <<-EOF
-				bind_addr = "${BIND_ADDR}"
-			EOF
+		cat > "${EP_CONF_DIR}/bind-addr.hcl" <<-EOF
+			bind_addr = "${BIND_ADDR}"
+		EOF
 	fi
 
 	if [ -n "${ADVERTISE_ADDR-}" ]; then
-			cat > "${EP_CONF_DIR}/advertise-addr.hcl" <<-EOF
-				advertise {
-				  http = "${ADVERTISE_ADDR}"
-				  rpc = "${ADVERTISE_ADDR}"
-				  serf = "${ADVERTISE_ADDR}"
-				}
-			EOF
+		cat > "${EP_CONF_DIR}/advertise-addr.hcl" <<-EOF
+			advertise {
+			  http = "${ADVERTISE_ADDR}"
+			  rpc = "${ADVERTISE_ADDR}"
+			  serf = "${ADVERTISE_ADDR}"
+			}
+		EOF
 	fi
 
 	if [ -n "${DRIVER_ALLOWLIST-}" ]; then
-			cat > "${EP_CONF_DIR}/driver-allow.hcl" <<-EOF
-				client {
-				  options {
-				    "driver.allowlist" = "${DRIVER_ALLOWLIST}"
-				  }
-				}
-			EOF
+		cat > "${EP_CONF_DIR}/driver-allow.hcl" <<-EOF
+			client {
+			  options {
+			    "driver.allowlist" = "${DRIVER_ALLOWLIST}"
+			  }
+			}
+		EOF
 	fi
 
 	if [ -z "${TLS_DISABLED:+1}" ]; then
-			cat > "${EP_CONF_DIR}/tls.hcl" <<-EOF
-				tls {
-				  http = true
-				  rpc = true
-				  verify_https_client = ${TLS_HTTPS_VERIFY:-true}
-				  verify_server_hostname = true
-				  ca_file = "${TLS_CA_FILE:?'TLS_CA_FILE must be set!'}"
-				  cert_file = "${TLS_CERT_FILE:?'TLS_CERT_FILE must be set!'}"
-				  key_file = "${TLS_KEY_FILE:?'TLS_KEY_FILE must be set!'}"
-				}
-			EOF
+		cat > "${EP_CONF_DIR}/tls.hcl" <<-EOF
+			tls {
+			  http = true
+			  rpc = true
+			  verify_https_client = ${TLS_HTTPS_VERIFY:-true}
+			  verify_server_hostname = true
+			  ca_file = "${TLS_CA_FILE:?'TLS_CA_FILE must be set!'}"
+			  cert_file = "${TLS_CERT_FILE:?'TLS_CERT_FILE must be set!'}"
+			  key_file = "${TLS_KEY_FILE:?'TLS_KEY_FILE must be set!'}"
+			}
+		EOF
 	fi
 
 	if [ $is_server ] && [ -z "${SERF_ENC_DISABLED:+1}" ]; then
-			: "${SERF_ENC_KEY:?'SERF_ENC_KEY must be set!'}"
-			# mktemp creates with mode 600
-			f=$(mktemp -p "${EP_CONF_DIR}")
-			cat > "$f" <<-EOF
-				server {
-				  encrypt = "${SERF_ENC_KEY}"
-				}
-			EOF
-			mv "$f" "${EP_CONF_DIR}/serf-enc-key.hcl"
+		: "${SERF_ENC_KEY:?'SERF_ENC_KEY must be set!'}"
+		# mktemp creates with mode 600
+		f=$(mktemp -p "${EP_CONF_DIR}")
+		cat > "$f" <<-EOF
+			server {
+			  encrypt = "${SERF_ENC_KEY}"
+			}
+		EOF
+		mv "$f" "${EP_CONF_DIR}/serf-enc-key.hcl"
 	fi
 
 	if [ -n "${ACL_ENABLED:+1}" ]; then
-			cat > "${EP_CONF_DIR}/acl-enable.hcl" <<-EOF
-				acl {
-				  enabled = true
-				}
-			EOF
+		cat > "${EP_CONF_DIR}/acl-enable.hcl" <<-EOF
+			acl {
+			  enabled = true
+			}
+		EOF
 	fi
 
 	if [ -n "${NOMAD_SERVERS-}" ]; then
-			cat > "${EP_CONF_DIR}/servers.hcl" <<-EOF
-				client {
-				  servers = [$(
-						echo "${NOMAD_SERVERS}" \
-						| tr , ' ' \
-						| xargs printf '"%s",' \
-						| sed 's/,$//'
-					)]
-				}
-			EOF
+		cat > "${EP_CONF_DIR}/servers.hcl" <<-EOF
+			client {
+			  servers = [$(
+					echo "${NOMAD_SERVERS}" \
+					| tr , ' ' \
+					| xargs printf '"%s",' \
+					| sed 's/,$//'
+				)]
+			}
+		EOF
 	fi
 }
 
