3c49f0d6f5fa757806f06e9c808ade455eb50b3f
Reuben Lifshay <rlifshay@gmail.com>
2026-04-06 00:47:55 -0700

    feat: disable selinux labeling for nomad alias

--- alias.sh
+++ alias.sh
@@ -1,2 +1,2 @@
 # example alias to run the cli more easily
-alias nomad='podman run --rm -i --net=host --env "NOMAD_*" -v .:/workdir -w /workdir ghcr.io/computator/nomad'
+alias nomad='podman run --rm -i --net=host --env "NOMAD_*" --security-opt=label=disable -v .:/workdir -w /workdir ghcr.io/computator/nomad'
